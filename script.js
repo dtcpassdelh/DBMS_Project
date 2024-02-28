031025fb0239d1bba41e4c6b0f8631129631ccad
rahul07tiwari <rahultiwari16031@gmail.com>
2024-02-28 07:28:30 +0530

    commited

---
+++ script.js
@@ -0,0 +1,69 @@
+/*console.log("Welcome to my first page");
+console.log("variables");
+//console.log("it is dynamically typed meaning variable datatype doesn't matter");
+// assignment operators "="
+// variables are case sensitive
+// in variable space not allowed
+//reserved word cannot be used eg console  
+AGE=21
+age=21;
+y= null;
+x= undefined;
+/*console="Rahul";
+console.log(console);*/ //reserved word
+// camel case used first word small
+/*console.log(x);
+console.log(y);
+console.log(age);
+isFollow=true;
+console.log(isFollow);
+console.log(AGE);*/
+/* var: variable can be re-declared and updated.A global scope variable
+    let: variable cannot be re-declared but can be updated.A block scope variable
+    const: variable cannot be re-declared or updated. A block scope variable.
+    block scope means { }
+
+let fullname= "rahul";
+console.log(fullname);
+var fullName="Rahul";
+console.log(fullName);
+var fullName="rohit"; // can be redeclare
+console.log(fullName)
+/*let fullname="raj"
+console.log(fullname); cannot redeclare in a block*/
+/*{const pi = 3.14; // always constant in a block
+console.log(pi);}// can be redefined in new block 
+let a;
+console.log(a);// can be redefined in new block
+const b;
+console.log(b); error
+
+console.log("Data Types");
+*/
+// Number,String, Boolean, Undefined, Null, Bigint, symbol
+/*primitive    :  
+   non primitive:  
+   objects      :  function, arrays // key : value
+   
+  const student = {
+    age : 21,
+    name : "rahul",
+    cgpa : 8.0,
+    isPass : true
+   };
+student["age"]=student["age"] + 1;
+student["name"]= "Rahul Tiwari"
+console.log(student.age);
+console.log(student["name"]);//variable name
+*/
+/*
+const profile={
+    idname : "Rahul07Tiwari",
+    post : 195,
+    followers :559,
+    following : 4,
+    fullname : "Rahul Tiwari",
+    bio : "student DSEU Rajokri Campus"
+};
+console.log(typeof profile["idname"]);
+*/
